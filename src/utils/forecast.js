7e2f3a81789069686c42865f99044b4a8fd21ab2
Samantha Lau <samantha.lhe@gmail.com>
2019-04-17 22:42:25 +0800

    Fix forecast api coordinate order and updated error message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,14 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, cb) => {
-  const url = `https://api.darksky.net/forecast/a88b4e021a5c80416f99a03a6ecaca3b/${latitude},${longitude}?units=si`
+  const url = `https://api.darksky.net/forecast/a88b4e021a5c80416f99a03a6ecaca3b/${longitude},${latitude}?units=si`
 
 
   request({url, json:true}, (error, {body}) => {
     if(error) {
       cb('Unable to connect to weather service')
     } else if(body.error) {
-      cb('Unable to find location')
+      cb('Unable to find weather for that location')
     } else {
       cb(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
     }
